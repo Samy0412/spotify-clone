c078fdb65c82f48e04ee8cd8df1d29c5bccccb6d
Samy0412 <samantha.gadet@gmail.com>
2020-11-18 12:01:41 -0500

    redirect to home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
+import React, {useEffect, useState} from "react";
+import { getTokenFromUrl } from "../spotify";
 import '../styles/App.scss';
 import Login from './Login';
 
 function App() {
+  const [token, setToken]= useState(null);
+
+  //Run code based on a given condition
+  useEffect(()=> {
+    const hash = getTokenFromUrl();
+    window.location.hash ="";
+    console.log('I HAVE A TOKEN>>>>',hash)
+
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+    }
+
+  },[]);
+
   return (
     <div className="app">
-     <Login/>
+      {
+        token ? (
+          <div>i am logged in</div>
+        ): (
+          <Login/>
+        )
+      }
+     
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -15,4 +15,17 @@ const scopes = [
   "user-modify-playback-state",
 ];
 
+export const getTokenFromUrl =()=> {
+  return window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce((initial,item)=> {
+
+    let parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+
+    return initial
+  }, {});
+}
+
 export const loginUrl = `${authEndpoint}?response_type=token&client_id=${clientId}&scope=${scopes.join("%20")}&redirect_uri=${redirectUri}&show_dialog=true`;
