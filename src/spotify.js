027592fedc22ff583a6bc2b9aa169477b710f837
Samy0412 <samantha.gadet@gmail.com>
2020-11-18 10:53:09 -0500

    spotify authentication done

---
+++ src/spotify.js
@@ -0,0 +1,18 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+
+export const authEndpoint = "https://accounts.spotify.com/authorize"
+
+const redirectUri = "http://localhost:3001/";
+
+const clientId = "ca189fea68e447d89c64f2d16f464333";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const loginUrl = `${authEndpoint}?response_type=token&client_id=${clientId}&scope=${scopes.join("%20")}&redirect_uri=${redirectUri}&show_dialog=true`;
