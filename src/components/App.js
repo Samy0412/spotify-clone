d89f63508ad006f594d6d3feab0ad13c4abf705a
Samy0412 <samantha.gadet@gmail.com>
2020-11-20 18:11:25 -0500

    reorganisation of files

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React, {useEffect, useState} from "react";
-import { getTokenFromUrl } from "../spotify";
+import { getTokenFromUrl } from "./spotify";
 import '../styles/App.scss';
 import Login from './Login';
+import SpotifyWebApi from "spotify-web-api-js"
+import Player from "./Player";
+
+const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken]= useState(null);
@@ -16,6 +20,12 @@ function App() {
 
     if(_token){
       setToken(_token)
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log(user)
+      })
     }
 
   },[]);
@@ -24,7 +34,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <div>i am logged in</div>
+          <Player/>
         ): (
           <Login/>
         )
